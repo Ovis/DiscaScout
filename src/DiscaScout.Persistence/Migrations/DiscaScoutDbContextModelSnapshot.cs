e44f5661d98c5750f69bb29ccd3ae50aea26427a
Ovis <3971624+Ovis@users.noreply.github.com>
2026-08-30 00:21:17 +0900

    Update model snapshot for Discord settings

--- src/DiscaScout.Persistence/Migrations/DiscaScoutDbContextModelSnapshot.cs
+++ src/DiscaScout.Persistence/Migrations/DiscaScoutDbContextModelSnapshot.cs
@@ -14,6 +14,6 @@ public sealed class DiscaScoutDbContextModelSnapshot : ModelSnapshot
     /// <inheritdoc />
     protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        InitialCatalogReviewModelBuilder.Build(modelBuilder);
+        DiscordNotificationSettingsModelBuilder.Build(modelBuilder);
     }
 }
