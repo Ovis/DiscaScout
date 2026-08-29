566dafc0757435024b915518f9367e285030389f
Ovis <3971624+Ovis@users.noreply.github.com>
2026-08-29 22:13:42 +0900

    Update model snapshot for disc detail metadata

--- src/DiscaScout.Persistence/Migrations/DiscaScoutDbContextModelSnapshot.cs
+++ src/DiscaScout.Persistence/Migrations/DiscaScoutDbContextModelSnapshot.cs
@@ -14,6 +14,6 @@ public sealed class DiscaScoutDbContextModelSnapshot : ModelSnapshot
     /// <inheritdoc />
     protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        ManualWorkModelBuilder.Build(modelBuilder);
+        DiscDetailMetadataModelBuilder.Build(modelBuilder);
     }
 }
